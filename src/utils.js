23550df7402a2ae13f754931188136488d5377b3
Jonnie Spratley <jonniespratley@gmail.com>
2016-09-28 14:23:50 -0700

    fix: fixed cmds

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,23 @@
 'use strict';
-const request = require('request');
-const fs = require('fs-extra');
+const assert = require('assert');
 const async = require('async');
-const path = require('path');
 const crypto = require('crypto');
+const debug = require('debug');
+const fs = require('fs-extra');
+const glob = require('glob');
+const request = require('request');
+
+const path = require('path');
+
 const pkg = require(path.resolve(__dirname, '../package.json'));
 const log = require('npmlog');
-const debug = require('debug');
 const logger = debug(`${pkg.name}:utils`);
 
+const zip = new require('node-zip')();
 const shell = require('pshell').context({
-    echoCommand: false,
-    ignoreError: false,
-    captureOutput: false
+  echoCommand: false,
+  ignoreError: false,
+  captureOutput: false
 });
 
 
@@ -22,119 +27,119 @@ const shell = require('pshell').context({
  * @description Utility methods used by passbook-cli.
  */
 class Utils {
-    constructor() {}
+  constructor() {}
 
-    /**
-     * exec - description
-     *
-     * @param  {type} cmd     description
-     * @param  {type} options description
-     * @return {type}         description
-     */
-    exec(cmd, options) {
-        return shell(cmd, options);
-    }
+  /**
+   * exec - description
+   *
+   * @param  {type} cmd     description
+   * @param  {type} options description
+   * @return {type}         description
+   */
+  exec(cmd, options) {
+    return shell(cmd, options);
+  }
 
-    /**
-     * checksum - description
-     *
-     * @param  {type} str       description
-     * @param  {type} algorithm description
-     * @param  {type} encoding  description
-     * @return {type}           description
-     */
-    checksum(str, algorithm, encoding) {
-        return crypto
-            .createHash(algorithm || 'md5')
-            .update(str, 'utf8')
-            .digest(encoding || 'hex');
-    }
+  /**
+   * checksum - description
+   *
+   * @param  {type} str       description
+   * @param  {type} algorithm description
+   * @param  {type} encoding  description
+   * @return {type}           description
+   */
+  checksum(str, algorithm, encoding) {
+    return crypto
+      .createHash(algorithm || 'md5')
+      .update(str, 'utf8')
+      .digest(encoding || 'hex');
+  }
 
-    /**
-     * getLogger - description
-     *
-     * @param  {type} name description
-     * @return {type}      description
-     */
-    getLogger(name) {
-            return debug(pkg.name + ':' + name);
-        }
+  /**
+   * getLogger - description
+   *
+   * @param  {type} name description
+   * @return {type}      description
+   */
+  getLogger(name) {
+    return debug(pkg.name + ':' + name);
+  }
 
-    /**
-     * createPassAssets - Copy pass assets from ./templates dir to pass destination.
-     *
-     * @param  {Object} obj Options object
-     * @param  {String} obj.name name of the pass
-     * @param  {String} obj.type type of pass
-     * @param  {String} obj.dest output path
-     * @param  {String} obj.passTypeIdentifier pass type identifier to use
-     * @param  {String} obj.teamIdentifier team identifier to use
-     * @param  {String} obj.pass pass object to mixin with defaults
-     * @return {String} path The path of the output
-     */
-    createPassAssets(obj) {
-        return new Promise((resolve, reject) => {
-            let name = obj.name;
-            let passType = obj.type;
-            let dest = obj.dest;
-            let passObj = obj.pass || {};
+  /**
+   * createPassAssets - Copy pass assets from ./templates dir to pass destination.
+   *
+   * @param  {Object} obj Options object
+   * @param  {String} obj.name name of the pass
+   * @param  {String} obj.type type of pass
+   * @param  {String} obj.dest output path
+   * @param  {String} obj.passTypeIdentifier pass type identifier to use
+   * @param  {String} obj.teamIdentifier team identifier to use
+   * @param  {String} obj.pass pass object to mixin with defaults
+   * @return {String} path The path of the output
+   */
+  createPassAssets(obj) {
+    return new Promise((resolve, reject) => {
+      let name = obj.name;
+      let passType = obj.type;
+      let dest = obj.dest;
+      let passObj = obj.pass || {};
 
-            let destDir = path.resolve(dest, `./${name}.raw/`);
-            let templatesDir = path.resolve(__dirname, `./templates/${passType}.raw/`);
-            let passTypeIdentifier = obj.passTypeIdentifier || process.env.PASS_TYPE_IDENTIFIER;
-            let teamIdentifier = obj.teamIdentifier || process.env.TEAM_IDENTIFIER;
-            var passJson = fs.readJsonSync(`${templatesDir}/pass.json`);
-            passObj = Object.assign(passJson, passObj);
-            if (passTypeIdentifier) {
-                logger('using', passTypeIdentifier);
-                passJson.passTypeIdentifier = passTypeIdentifier;
-            }
-            if (teamIdentifier) {
-                logger('using', teamIdentifier);
-                passJson.teamIdentifier = teamIdentifier;
-            }
+      let destDir = path.resolve(dest, `./${name}.raw/`);
+      let templatesDir = path.resolve(__dirname, `./templates/${passType}.raw/`);
+      let passTypeIdentifier = obj.passTypeIdentifier || process.env.PASS_TYPE_IDENTIFIER;
+      let teamIdentifier = obj.teamIdentifier || process.env.TEAM_IDENTIFIER;
+      var passJson = fs.readJsonSync(`${templatesDir}/pass.json`);
+      passObj = Object.assign(passJson, passObj);
+      if (passTypeIdentifier) {
+        logger('using', passTypeIdentifier);
+        passJson.passTypeIdentifier = passTypeIdentifier;
+      }
+      if (teamIdentifier) {
+        logger('using', teamIdentifier);
+        passJson.teamIdentifier = teamIdentifier;
+      }
 
-            //log.info('pass.json', passJson);
-            logger('createPassAssets', 'from', templatesDir);
-            logger('createPassAssets', 'to', destDir);
-            try {
-                fs.ensureDirSync(destDir);
-                fs.copySync(templatesDir, destDir);
-                fs.outputJsonSync(`${destDir}/pass.json`, passObj);
-                fs.removeSync(`${destDir}/.DS_Store`);
-                resolve(destDir);
-            } catch (e) {
-                logger('createPassAssets', e);
-                reject(e);
-            }
-        });
-    }
+      //log.info('pass.json', passJson);
+      logger('createPassAssets', 'from', templatesDir);
+      logger('createPassAssets', 'to', destDir);
+      try {
+        fs.ensureDirSync(destDir);
+        fs.copySync(templatesDir, destDir);
+        fs.outputJsonSync(`${destDir}/pass.json`, passObj);
+        fs.removeSync(`${destDir}/.DS_Store`);
+        resolve(destDir);
+      } catch (e) {
+        logger('createPassAssets', e);
+        reject(e);
+      }
+    });
+  }
 
-    /**
-     * getPkcs12CertCmd - Creates the command needed to generate a certificate.pem
-     *
-     * @param  {String} p12           path to the .p12 cert
-     * @param  {String} passphrase    passphrase for .p12 cert
-     * @return {Object} cmd         the command and filename
-     */
-    getPkcs12CertCmd(p12, passphrase) {
-        let _path = path.parse(p12);
-        logger('getPkcs12CertCmd', p12);
-        _path.ext = '.pem';
-        _path.name += '-cert';
-        let _out = p12.replace('.p12', '-cert.pem');
-        let _cmd =
-            `openssl pkcs12 \
+  /**
+   * getPkcs12CertCmd - Creates the command needed to generate a certificate.pem
+   *
+   * @param  {String} p12           path to the .p12 cert
+   * @param  {String} passphrase    passphrase for .p12 cert
+   * @return {Object} cmd         the command and filename
+   */
+  getPkcs12CertCmd(p12, passphrase) {
+      let _path = path.parse(p12);
+      logger('getPkcs12CertCmd', p12);
+      _path.ext = '.pem';
+      _path.name += '-cert';
+      let _out = p12.replace('.p12', '-cert.pem');
+      let _cmd =
+        `openssl pkcs12 \
       -in ${p12} \
       -passin pass:${passphrase} \
       -clcerts \
       -nokeys \
       -out ${_out}`;
-        logger('getPkcs12CertCmd', _cmd);
-        return {
-            filename: _out,
-            cmd: _cmd
-        };
+      logger('getPkcs12CertCmd', _cmd);
+      return {
+        filename: _out,
+        cmd: _cmd
+      };
     }
     /**
      * getPkcs12KeyCmd - Creates the command needed to generate a key.pem
@@ -143,81 +148,145 @@ class Utils {
      * @param  {String} passphrase    passphrase for .p12 key
      * @return {Object} cmd           the command and filename
      */
-    getPkcs12KeyCmd(p12, passphrase) {
-        let _path = path.parse(p12);
-        _path.ext = '.pem';
-        _path.name += '-key';
-        p12 = path.resolve(p12);
+  getPkcs12KeyCmd(p12, passphrase) {
+    let _path = path.parse(p12);
+    _path.ext = '.pem';
+    _path.name += '-key';
+    p12 = path.resolve(p12);
 
-        logger('getPkcs12KeyCmd', p12);
+    logger('getPkcs12KeyCmd', p12);
 
 
-        let _out = p12.replace('.p12', '-key.pem');
-        let _cmd =
-            `openssl pkcs12 -in ${p12} -nocerts -passout pass:${passphrase} -passin pass:${passphrase} -out ${_out}`;
-        logger('getPkcs12KeyCmd', _cmd);
-        return {
-            filename: _out,
-            cmd: _cmd
-        };
-    }
+    let _out = p12.replace('.p12', '-key.pem');
+    let _cmd =
+      `openssl pkcs12 -in ${p12} -nocerts -passout pass:${passphrase} -passin pass:${passphrase} -out ${_out}`;
+    logger('getPkcs12KeyCmd', _cmd);
+    return {
+      filename: _out,
+      cmd: _cmd
+    };
+  }
 
-    /**
-     * createPemFiles - Creates the required key/cert for signing a .raw package into a .pkpass
-     *
-     * @param  {type} p12        The path to the .p12 cert
-     * @param  {type} passphrase The passphrase for the cert
-     * @param  {type} dest       The output destination (default is .p12 dir)
-     * @return {type}            description
-     */
-    createPemFiles(p12, passphrase) {
+  /**
+   * createPemFiles - Creates the required key/cert for signing a .raw package into a .pkpass
+   *
+   * @param  {type} p12        The path to the .p12 cert
+   * @param  {type} passphrase The passphrase for the cert
+   * @param  {type} dest       The output destination (default is .p12 dir)
+   * @return {type}            description
+   */
+  createPemFiles(p12, passphrase) {
 
-        return new Promise((resolve, reject) => {
-            let key = this.getPkcs12KeyCmd(p12, passphrase);
-            let cert = this.getPkcs12CertCmd(p12, passphrase);
-            Promise.all([
-                this.exec(key.cmd),
-                this.exec(cert.cmd)
-            ]).then((res) => {
-                resolve([
-                    key, cert
-                ]);
-            }).catch(reject);
-
+    return new Promise((resolve, reject) => {
+      let key = this.getPkcs12KeyCmd(p12, passphrase);
+      let cert = this.getPkcs12CertCmd(p12, passphrase);
+      Promise.all([
+        this.exec(key.cmd),
+        this.exec(cert.cmd)
+      ]).then((res) => {
+        resolve({
+          key, cert
         });
-    }
+      }).catch(reject);
+
+    });
+  }
 
 
-    /**
-     * validatePkpass - Validate the contents of a .pkpass package.
-     *
-     * @param  {String} pkpassFilename The path to the .pkpass
-     * @return {String}                description
-     */
-    validatePkpass(pkpassFilename) {
-        assert(pkpassFilename, 'has pkpass filename');
-        let bin = path.resolve(__dirname, './bin/signpass');
-        let regex = /(FAILED)/gmi;
-        logger('validate', pkpassFilename);
-        return new Promise((resolve, reject) => {
-            shell(`${bin} -v ${pkpassFilename}`, {
-                captureOutput: (buf) => {
-                    return buf.toString();
-                }
-            }).then((resp) => {
-                logger('resp', resp);
-                if (regex.test(resp.stdout)) {
-                    logger('err', resp);
-                    reject(new Error(resp.stdout));
-                } else {
-                    resolve(resp.stdout);
-                }
-            }).catch((err) => {
-                logger('err', err);
-                reject(err);
-            });
-        });
-    }
+  /**
+   * validatePkpass - Validate the contents of a .pkpass package.
+   *
+   * @param  {String} pkpassFilename The path to the .pkpass
+   * @return {String}                description
+   */
+  validatePkpass(pkpassFilename) {
+    assert(pkpassFilename, 'has pkpass filename');
+    let bin = path.resolve(__dirname, './bin/signpass');
+    let regex = /(FAILED)/gmi;
+    logger('validate', pkpassFilename);
+    return new Promise((resolve, reject) => {
+      shell(`${bin} -v ${pkpassFilename}`, {
+        captureOutput: (buf) => {
+          return buf.toString();
+        }
+      }).then((resp) => {
+        logger('resp', resp);
+        if (regex.test(resp.stdout)) {
+          logger('err', resp);
+          reject(new Error(resp.stdout));
+        } else {
+          resolve(resp.stdout);
+        }
+      }).catch((err) => {
+        logger('err', err);
+        reject(err);
+      });
+    });
+  }
+
+  generateJsonManifest(rawpassFilename) {
+    return new Promise((resolve, reject) => {
+      let _manifest = {};
+      let _manifestFilename = path.resolve(rawpassFilename, './manifest.json');
+      let _filename, _checksum;
+
+      logger('generate_json_manifest', _manifestFilename);
+
+      fs.removeSync(path.resolve(rawpassFilename, './.DS_Store'));
+      glob(`${rawpassFilename}/**/*.*`, (err, files) => {
+        if (err) {
+          reject(err);
+        }
+
+        if (files && files.length) {
+          files.forEach((file) => {
+            logger('Check if file is in manifest', file);
+            _filename = file.replace(rawpassFilename + path.sep, '');
+            _checksum = this.checksum(fs.readFileSync(file), 'sha1');
+            _manifest[_filename] = _checksum;
+
+            logger('checksum', _filename);
+          });
+          fs.writeFile(_manifestFilename, JSON.stringify(_manifest), (err) => {
+            if (err) {
+              reject(err);
+            }
+            logger('generate_json_manifest', 'complete');
+            resolve(_manifestFilename);
+          });
+        }
+      });
+    });
+  }
+
+  signJsonManifest(rawpassFilename, cert, key, passphrase) {
+    return new Promise((resolve, reject) => {
+      let wwdr = path.resolve(__dirname, './certificates/wwdr-authority.pem');
+      let manifest = path.resolve(rawpassFilename, './manifest.json');
+      let signedContents;
+      let signature = path.resolve(rawpassFilename, './signature');
+      logger('sign_manifest', signature);
+      let cmd =
+        `openssl smime -binary \
+  				-sign \
+  				-certfile ${wwdr} \
+  				-signer ${cert} \
+  				-inkey ${key} \
+  				-in ${manifest} \
+  				-out ${signature} \
+  				-outform DER \
+  				-passin pass:${passphrase}`;
+
+      //logger('sign_manifest', sign_pass_cmd);
+      this.exec(cmd).then((res) => {
+        logger('sign_manifest', res);
+        resolve(signature);
+      }).catch((error) => {
+        log.error('sign_manifest', error);
+        reject(error);
+      });
+    });
+  }
 }
 
 
--- test/utils-spec.js
+++ test/utils-spec.js
@@ -8,126 +8,149 @@ const tempDir = path.resolve(__dirname, './temp');
 const pem = require('pem');
 const config = require('./test-config');
 
-
+var rawpassFilename;
 describe('Utils', () => {
-    it('getLogger - returns log instance', (done) => {
-        var logger = utils.getLogger('test');
-        assert(logger);
-        logger('spec');
+  it('getLogger - returns log instance', (done) => {
+    var logger = utils.getLogger('test');
+    assert(logger);
+    logger('spec');
+    done();
+  });
+
+  it('checksum - returns checksum', (done) => {
+    var str = utils.checksum('test');
+    assert(str);
+    done();
+  });
+
+  it('exec - executes command and returns promise', (done) => {
+    utils.exec('node --version').then((resp) => {
+      assert(resp);
+      done();
+    });
+  });
+
+  describe('certificate utils', () => {
+    var cmds = [];
+    it('getPkcs12CertCmd - returns filename and command', (done) => {
+      var str = utils.getPkcs12CertCmd(config.certs.p12, 'test', tempDir);
+      assert(str);
+      cmds.push(str);
+      done();
+    });
+
+    it('getPkcs12KeyCmd - returns filename and command', (done) => {
+      var str = utils.getPkcs12KeyCmd(config.certs.p12, 'test', tempDir);
+      assert(str);
+      cmds.push(str);
+
+      done();
+    });
+
+    it('createPemFiles - runs command and files', (done) => {
+      utils.createPemFiles(config.certs.p12, config.certs.passphrase, tempDir).then((res) => {
+        console.log(res);
+        config.certs.cert = res.cert;
+        config.certs.key = res.key;
+        assert(res);
         done();
+      }).catch((err) => {
+        done(err);
+      });
     });
 
-    it('checksum - returns checksum', (done) => {
-        var str = utils.checksum('test');
-        assert(str);
+    xit('should execute cert command', (done) => {
+      let _done = _.after(cmds.length, () => {
         done();
+      });
+      _.forEach(cmds, (cmd) => {
+        assert(cmd);
+        utils.exec(cmd.cmd).then((resp) => {
+          assert(resp);
+          assert(fs.existsSync(cmd.filename), 'creates filename');
+          _done();
+        }).catch((err) => {
+          done(err);
+        });
+      });
     });
 
-    it('exec - executes command and returns promise', (done) => {
-        utils.exec('node --version').then((resp) => {
-            assert(resp);
-            done();
-        });
+  });
+
+  context('Passes', () => {
+    it('createPassAssets - should create .raw pass files to dest and resolve', (done) => {
+      utils.createPassAssets({
+        name: 'test-pass',
+        type: 'generic',
+        dest: tempDir
+      }).then((resp) => {
+        assert(resp);
+        done();
+      }).catch((err) => {
+        assert.fail(err);
+        done();
+      });
     });
 
-    describe('certificate utils', () => {
-        var cmds = [];
-        it('getPkcs12CertCmd - returns filename and command', (done) => {
-            var str = utils.getPkcs12CertCmd(config.certs.p12, 'test', tempDir);
-            assert(str);
-            cmds.push(str);
-            done();
-        });
-
-        it('getPkcs12KeyCmd - returns filename and command', (done) => {
-            var str = utils.getPkcs12KeyCmd(config.certs.p12, 'test', tempDir);
-            assert(str);
-            cmds.push(str);
-
-            done();
-        });
-
-        it('createPemFiles - runs command and files', (done) => {
-            utils.createPemFiles(config.certs.p12, config.certs.passphrase, tempDir).then((res) => {
-                assert(res);
-                done();
-            }).catch((err) => {
-                done(err);
-            });
-        });
-
-        xit('should execute cert command', (done) => {
-            let _done = _.after(cmds.length, () => {
-                done();
-            });
-            _.forEach(cmds, (cmd) => {
-                assert(cmd);
-                utils.exec(cmd.cmd).then((resp) => {
-                    assert(resp);
-                    assert(fs.existsSync(cmd.filename), 'creates filename');
-                    _done();
-                }).catch((err) => {
-                    done(err);
-                });
-            });
-        });
-
+    it('createPassAssets - should create .raw pass files, extended with passes obj and resolve', (done) => {
+      utils.createPassAssets({
+        name: 'test-pass',
+        type: 'generic',
+        teamIdentifier: '12345',
+        passTypeIdentifier: 'pass.io.test',
+        dest: tempDir
+      }).then((resp) => {
+        let pass = JSON.parse(fs.readFileSync(`${resp}/pass.json`));
+        rawpassFilename = resp;
+        assert(resp);
+        assert(fs.existsSync(resp));
+        assert(pass.teamIdentifier === '12345');
+        assert(pass.passTypeIdentifier === 'pass.io.test');
+        done();
+      }).catch((err) => {
+        console.log('err', err);
+        assert.fail(err);
+        done();
+      });
     });
 
-    context('Passes', ()=>{
-        it('createPassAssets - should create .raw pass files to dest and resolve', (done) => {
-            utils.createPassAssets({
-                name: 'test-pass',
-                type: 'generic',
-                dest: tempDir
-            }).then((resp) => {
-                assert(resp);
-                done();
-            }).catch((err) => {
-                assert.fail(err);
-                done();
-            });
-        });
-
-        it('createPassAssets - should create .raw pass files, extended with passes obj and resolve', (done) => {
-            utils.createPassAssets({
-                name: 'test-pass',
-                type: 'generic',
-                teamIdentifier: '12345',
-                passTypeIdentifier: 'pass.io.test',
-                dest: tempDir
-            }).then((resp) => {
-                let pass = JSON.parse(fs.readFileSync(`${resp}/pass.json`));
-                assert(resp);
-                assert(fs.existsSync(resp));
-                assert(pass.teamIdentifier === '12345');
-                assert(pass.passTypeIdentifier === 'pass.io.test');
-                done();
-            }).catch((err) => {
-                console.log('err', err);
-                assert.fail(err);
-                done();
-            });
-        });
-
-        it('validatePkpass', (done) =>{
-
-        });
+    it('validatePkpass', (done) => {
+      done();
     });
-
-    describe('pem', () => {
-        it('readPkcs12 - should read cert', (done) => {
-            var p12Path = config.certs.p12;
-            var options = {
-                clientKeyPassword: config.certs.passphrase,
-                p12Password: config.certs.passphrase
-            };
-            //console.log('pem', p12Path);
-            pem.readPkcs12(p12Path, options, (resp) => {
-                console.log(resp);
-                done();
-            });
-        });
+    it('generateJsonManifest', (done) => {
+      utils.generateJsonManifest(rawpassFilename).then((resp) => {
+        assert(resp);
+        done();
+      }).catch((err) => {
+        assert.fail(err);
+        done();
+      });
     });
+    it('signJsonManifest', (done) => {
+      utils.generateJsonManifest(rawpassFilename, config.certs.cert.filename, config.certs.key.filename,
+        config.certs.passphrase).then((resp) => {
+        assert(resp);
+        done();
+      }).catch((err) => {
+        assert.fail(err);
+        done();
+      });
+    });
+  });
+
+  describe('pem', () => {
+    it('readPkcs12 - should read cert', (done) => {
+      var p12Path = config.certs.p12;
+      var options = {
+        clientKeyPassword: config.certs.passphrase,
+        p12Password: config.certs.passphrase
+      };
+      //console.log('pem', p12Path);
+      pem.readPkcs12(p12Path, options, (resp) => {
+        console.log(resp);
+        done();
+      });
+    });
+  });
 
 });
